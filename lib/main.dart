2582fa5fa2a445e654ad33fdedaeaa69b1a2695e
Roel Leal <50306254+RoelLeal@users.noreply.github.com>
2022-10-27 22:49:39 -0500

    BOTONES COMO INTENTS

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(home: MyTextfield()));
+  runApp(new MaterialApp(home: Route()));
 }
 
-class MyTextfield extends StatefulWidget {
-  const MyTextfield({super.key});
-
-  @override
-  State<MyTextfield> createState() => _MyTextfieldState();
-}
-
-class _MyTextfieldState extends State<MyTextfield> {
-  String _inputValue = "", inputValue = "";
-  final TextEditingController controller = new TextEditingController();
-  void onSubmitted(String value) {
-    setState(() {
-      inputValue = inputValue + "\n" + value;
-    });
-    controller.clear();
-    _showAlert(value);
-  }
-
-  void _showAlert(value) {
-    AlertDialog alert = new AlertDialog(
-      title: new Text("Advertencia"),
-      content: new Text("Eri gei?"),
-      actions: <Widget>[
-        new TextButton(
-            child: Text("OK"),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-        new TextButton(
-            child: Text("NO"),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-      ],
-    );
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
-  }
-
-  void onChange(value) {
-    setState(() {
-      _inputValue = value;
-    });
-  }
+class Route extends StatelessWidget {
+  const Route({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Textfields"),
-        backgroundColor: Colors.indigo.shade400,
+        title: new Text("Navigator"),
       ),
-      body: new Container(
-          padding: EdgeInsets.all(25.0),
-          child: new Center(
-              child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              new TextField(
-                decoration: new InputDecoration(hintText: "Ingresa un texto"),
-                controller: controller,
-                onChanged: (String value) {
-                  onChange(value);
-                },
-              ),
-              new ElevatedButton(
-                child: new Text("Presioname"),
-                onPressed: () {
-                  onSubmitted(_inputValue);
-                },
-              ),
-              new Text(
-                inputValue,
-                style:
-                    new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-              )
-            ],
-          ))),
+      body: new Center(
+          child: new ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: ((context) => second())));
+              },
+              child: new Text("Hola"))),
     );
+  }
+}
+
+class second extends StatelessWidget {
+  const second({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text("Second"),
+        ),
+        body: new Center(
+            child: new ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: ((context) => Route())));
+                },
+                child: new Text("Botoncito de vuelta"))));
   }
 }
